f8944d35740949e89c2ac9a68f695d4071dfebe7
Camrado <blackandwhite.b17w@gmail.com>
2023-12-22 14:36:27 +0400

    initial commit

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// unsigned integer means positive whole number or 0
+uint naturalNumber = 23;
+
+// integer means negative or positive whole number or 0
+int integerNumber = -23;
+
+// float means single-precision floating point. It is a 32-bit type that can represent numbers with a precision of approximately 7 digits.
+// F (or f) suffix makes it a float literal
+float realNumber = 2.3f;
+
+// double means double-precision floating point. It is a 64-bit type that can represent numbers with a precision of approximately 15-16 digits.
+// double is the default type for a number value with a decimal point.
+double anotherRealNumber = 2.3; // double literal
