af03f87491b20c9fca516029d2c680fdde66c60b
Camrado <blackandwhite.b17w@gmail.com>
2023-12-31 23:57:49 +0400

    Some part of Chapter03 is done

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 3;
+int b = a++; // make 'b' equal to 'a', and only then add 1 to 'a'
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c; // increment c before assigning it
+WriteLine($"c is {c}, d is {d}");
