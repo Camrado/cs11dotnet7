bd3b4bbd6a8b4778a4cd88931cdd9d6bbd99a4f4
Camrado <blackandwhite.b17w@gmail.com>
2024-01-10 17:48:42 +0400

    Finished Chapter 07

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using SharedLibrary;
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine(); // or "00ffc8"
+WriteLine($"Is {hex} a valid color? {hex.IsValidHex()}");
+
+Write("Enter an XML element: ");
+string? xmlTag = ReadLine(); // or "<h1 />"
+WriteLine($"Is {xmlTag} a valid XML element? {xmlTag.IsValidXmlTag()}");
+
+Write("Enter a password: ");
+string? password = ReadLine(); // or "secretsauce"
+WriteLine($"Is {password} a valid password? {password.IsValidPassword()}");
