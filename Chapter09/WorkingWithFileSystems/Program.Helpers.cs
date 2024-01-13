2403f409750ceecaf210a9f871f4622ed1336c3e
Camrado <blackandwhite.b17w@gmail.com>
2024-01-13 19:35:02 +0400

    Finished Chapter 09

---
+++ Chapter09/WorkingWithFileSystems/Program.Helpers.cs
@@ -0,0 +1,10 @@
+﻿partial class Program {
+    static void SectionTitle(string title) {
+        ConsoleColor previouscolor = ForegroundColor;
+        ForegroundColor = ConsoleColor.Yellow;
+        WriteLine("*");
+        WriteLine($"* {title}");
+        WriteLine("*");
+        ForegroundColor = previouscolor;
+    }
+}
