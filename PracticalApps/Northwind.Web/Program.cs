eb1439f5c611ca11b607992bae5a2d7419210fe1
Camrado <blackandwhite.b17w@gmail.com>
2024-01-21 13:36:02 +0400

    Finished Chapter 13

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -1,11 +1,12 @@
 using Packt.Shared; // AddNorthwindContext extension method
 
+// configure services
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddNorthwindContext();
-
 var app = builder.Build();
 
+// configure the HTTP pipeline
 if (!app.Environment.IsDevelopment()) {
     app.UseHsts();
 }
@@ -19,6 +20,7 @@ app.MapRazorPages();
 
 app.MapGet("/hello", () => "Hello World!");
 
+// start the web server
 app.Run();
 
 WriteLine("This executes after the web server has stopped!");
